bdc991c3ea816d2228d60d7a9bb168ff094dc385
roshiena <roshina@hotmail.com>
2022-10-12 09:56:48 +0800

    first commit

---
+++ TurnUp/TurnUp/Program.cs
@@ -0,0 +1,37 @@
+﻿// open chrome browser
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+
+
+// nagivate to TurnUp portal
+
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+driver.Manage().Window.Maximize();
+
+//Input username in username textbox
+
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+
+//Input password in password textbox
+
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+
+
+
+//Click Login
+
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click();
+
+//Homepage successfully
+
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+if (helloHari.Text == "Hello hari!")
+    Console.WriteLine("Login successfully,test passed");
+else
+    Console.WriteLine("Login Unsuccessful,test failed");
